2da8140bb7fd67d1dcf1e72fe1c88c723eba3ead
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2022-04-02 16:04:21 +0200

    Add subtitles to pillar headings

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,19 +8,19 @@ export default function Index() {
       </div>
       <div className="grid md:grid-cols-3">
         <article>
-          <h1 className="text-3xl text-center font-medium text-sky-500">
-            Bildung
-          </h1>
+          <h1 className="text-3xl font-medium text-sky-500">Bildung</h1>
+
+          <h2>Verstehen, begreigen, erklaeren</h2>
         </article>
         <article>
-          <h1 className="text-3xl text-center font-medium text-sky-500">
-            Persönlichkeit
-          </h1>
+          <h1 className="text-3xl font-medium text-sky-500">Praxiserfahrung</h1>
+          <h2>Dinge probieren</h2>
         </article>
         <article>
-          <h1 className="text-3xl text-center font-medium text-sky-500">
-            Praxis
+          <h1 className="text-3xl font-medium text-sky-500">
+            Persönlichkeitsentwicklung
           </h1>
+          <h2>Potenziale entfalten</h2>
         </article>
       </div>
     </div>
