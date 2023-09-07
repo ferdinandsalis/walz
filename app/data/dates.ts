fe0180931b39a36b8261fe03ef144270dc8a4ea2
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2023-09-07 11:01:49 +0200

    Fix dates

--- app/data/dates.ts
+++ app/data/dates.ts
@@ -8,73 +8,74 @@ export const dates = [
     title: 'Tag der offenen Tür',
     startDate: new Date('2023-10-07'),
     type: 'internal',
-    description: 'Die Walz stellt sich vor! Du kannst bei Workshops in Fremdsprachen, Projektpräsentationen und vielem mehr unseren Arbeitsalltag und auch die Besonderheiten der Walz kennenlernen. Am Abend findet noch eine Theateraufführung statt. Der Jahrgang Theta bringt „Einer flog übers Kuckucksnest“ auf die Bühne. Gerne führt dich auch ein Walzist oder eine Walzistin durch die Gebäude und beantwortet deine Fragen'
+    description:
+      'Die Walz stellt sich vor! Du kannst bei Workshops in Fremdsprachen, Projektpräsentationen und vielem mehr unseren Arbeitsalltag und auch die Besonderheiten der Walz kennenlernen. Am Abend findet noch eine Theateraufführung statt. Der Jahrgang Theta bringt „Einer flog übers Kuckucksnest“ auf die Bühne. Gerne führt dich auch ein Walzist oder eine Walzistin durch die Gebäude und beantwortet deine Fragen',
   },
   {
     title: 'Nationalfeiertag',
     startDate: new Date('2023-10-26'),
-    description: undefined
+    description: undefined,
   },
   {
-    title: 'Allerheiligen',
+    title: 'Allerheiligen (Beginn)',
     startDate: new Date('2023-11-01'),
     endDate: new Date('2023-11-02'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Maria Empfägnis',
     startDate: new Date('2023-12-08'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Infoabend',
     startDate: new Date('2023-11-23'),
     type: 'internal',
-    description: 'Von 19:00 bis 20:30 findet der erste von zwei Infoabenden statt. Interessierten Eltern stellen wir das pädagogische Konzept und das Programm der Walz vor, die Aufnahmevoraussetzungen und der Ablauf des Aufnahmetags wird erklärt und die Schulbeiträge werden erläutert. Natürlich bleibt auch Zeit für Fragen und Gespräche. Interessierte Jugendliche können selbstverständlich mitkommen.'
-
+    description:
+      'Von 19:00 bis 20:30 findet der erste von zwei Infoabenden statt. Interessierten Eltern stellen wir das pädagogische Konzept und das Programm der Walz vor, die Aufnahmevoraussetzungen und der Ablauf des Aufnahmetags wird erklärt und die Schulbeiträge werden erläutert. Natürlich bleibt auch Zeit für Fragen und Gespräche. Interessierte Jugendliche können selbstverständlich mitkommen.',
   },
   {
-    title: 'Weihnachtsferien',
+    title: 'Weihnachtsferien (Beginn)',
     startDate: new Date('2023-12-22'),
     endDate: new Date('2024-01-07'),
-    description: undefined
+    description: undefined,
   },
   {
-    title: 'Semesterferien',
+    title: 'Semesterferien (Beginn)',
     startDate: new Date('2024-02-05'),
     endDate: new Date('2024-02-11'),
-    description: undefined
+    description: undefined,
   },
   {
-    title: 'Osterferien',
+    title: 'Osterferien (Beginn)',
     startDate: new Date('2024-03-25'),
     endDate: new Date('2024-04-07'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Staatsfeiertag',
     startDate: new Date('2024-05-01'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Christi Himmelfahrt',
-    startDate: new Date('2024-05-01'),
-    description: undefined
+    startDate: new Date('2024-05-09'),
+    description: undefined,
   },
   {
-    title: 'Pfingsten',
+    title: 'Pfingsten (Beginn)',
     startDate: new Date('2023-05-20'),
     endDate: new Date('2023-05-21'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Fronleichnam',
     startDate: new Date('2024-05-30'),
-    description: undefined
+    description: undefined,
   },
   {
     title: 'Abschluss',
     startDate: new Date('2024-06-27'),
     description: undefined,
-  }
+  },
 ]
