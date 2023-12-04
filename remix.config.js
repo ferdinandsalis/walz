c2ada0aa64913714cfbdfd555d6c59ca3452160a
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2023-12-04 18:19:55 +0100

    Upgrade remix

--- remix.config.js
+++ remix.config.js
@@ -8,17 +8,7 @@ export default {
   ignoredRouteFiles: ['**/*'],
   serverModuleFormat: 'esm',
   serverPlatform: 'node',
-  tailwind: true,
-  postcss: true,
   watchPaths: ['./tailwind.config.ts'],
-  future: {
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    v2_dev: true,
-  },
   routes: async defineRoutes => {
     return flatRoutes('routes', defineRoutes, {
       ignoredRouteFiles: [
