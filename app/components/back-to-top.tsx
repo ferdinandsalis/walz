9da99625a744d32ec90ac40d6d3950da0181cb7b
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2024-01-07 09:00:09 -0700

    Add transition to top button

--- app/components/back-to-top.tsx
+++ app/components/back-to-top.tsx
@@ -5,7 +5,7 @@ export function BackToTop() {
     <div className="">
       <Link
         to="#"
-        className="inline-flex items-center gap-1 rounded-md border bg-card/30 p-2 px-6 font-condensed text-lg text-secondary"
+        className="inline-flex items-center gap-1 rounded-md border bg-card/50 p-2 px-6 font-condensed text-lg text-secondary transition-colors ease-in-out hover:bg-card"
       >
         <span className="">Nach oben</span>
       </Link>
