4717830e26776b6d82a6707f3384e0771070e33f
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2024-07-01 09:34:16 +0200

    Add new book of the year

--- app/routes/boty+/index.tsx
+++ app/routes/boty+/index.tsx
@@ -6,17 +6,30 @@ export default function Boty() {
       <h1 className="absolute left-0 origin-top-left rotate-90 scale-[4] font-condensed text-xl font-bold text-stone-500 opacity-10">
         Book of the year
       </h1>
-      <article>
-        <h2 className="font-condensed text-h3">22/23</h2>
-        <a
-          download="Book of the year 22/23.pdf"
-          href="/downloads/boty_22-23.pdf"
-          className="inline-flex items-center gap-1 underline underline-offset-2"
-        >
-          Jahrbuch herunterladen
-          <DownloadIcon size={18} className="stroke-primary" />
-        </a>{' '}
-      </article>
+      <div className="grid gap-4">
+        <article id="boty_23-24" className="rounded bg-card/50 p-6">
+          <h2 className="font-condensed text-h3">23/24</h2>
+          <a
+            download="Book of the year 23/24.pdf"
+            href="/downloads/boty_23-24.pdf"
+            className="inline-flex items-center gap-1 underline underline-offset-2"
+          >
+            Jahrbuch herunterladen
+            <DownloadIcon size={18} className="stroke-primary" />
+          </a>{' '}
+        </article>
+        <article id="boty_22-23" className="rounded bg-card/50 p-6">
+          <h2 className="font-condensed text-h3">22/23</h2>
+          <a
+            download="Book of the year 22/23.pdf"
+            href="/downloads/boty_22-23.pdf"
+            className="inline-flex items-center gap-1 underline underline-offset-2"
+          >
+            Jahrbuch herunterladen
+            <DownloadIcon size={18} className="stroke-primary" />
+          </a>{' '}
+        </article>
+      </div>
     </div>
   )
 }
