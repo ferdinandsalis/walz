d418283878f7b19dbb3a09fa0d57040354b56d70
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2026-03-08 08:19:50 +0100

    Add Alumni page GROQ query

---
+++ app/routes/alumni/query.ts
@@ -0,0 +1,54 @@
+import { defineQuery } from 'groq'
+import { z } from 'zod'
+import { PhotoSchema } from '#app/sanity/schema/year.tsx'
+import { PersonSchema } from '../ueber-uns+/_index.query.ts'
+
+export const alumniQuery = defineQuery(`{
+  "alumniYears": *[_type == "year" && defined(graduatedAt)] | order(graduatedAt desc) {
+    _id,
+    _type,
+    startedAt,
+    graduatedAt,
+    "plan": plan.asset->url,
+    letter,
+    mentor->{
+      familyName,
+      givenNames,
+      "name": givenNames + " " + familyName,
+      slug
+    },
+    photos[] {
+      _key,
+      takenAt,
+      motto,
+      caption,
+      attribution,
+      alt,
+      asset
+    } | order(takenAt desc),
+    featuredPhoto
+  }
+}`)
+
+export const YearSchema = z.object({
+  _id: z.string(),
+  _type: z.literal('year'),
+  letter: z.string(),
+  startedAt: z.coerce.date(),
+  graduatedAt: z.coerce.date().nullable(),
+  mentor: PersonSchema.pick({ givenNames: true, familyName: true })
+    .extend({
+      name: z.string(),
+      slug: z.object({ current: z.string() }),
+    })
+    .nullable(),
+  photos: z.array(PhotoSchema).nullable(),
+  plan: z.string().nullable(),
+  featuredPhoto: z.string().optional().nullable(),
+})
+
+export type Year = z.infer<typeof YearSchema>
+
+export type AlumniQueryResult = {
+  alumniYears: Year[]
+}
